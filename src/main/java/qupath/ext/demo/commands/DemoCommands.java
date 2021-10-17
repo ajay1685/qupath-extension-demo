5cde2dd1dbcfcc8910c8dfa33b7ae0a5263919c5
Ajay <ajay1685@hotmail.com>
2021-10-17 00:38:48 -0400

    Initial commit

---
+++ src/main/java/qupath/ext/demo/commands/DemoCommands.java
@@ -0,0 +1,41 @@
+package qupath.ext.demo.commands;
+
+import org.controlsfx.control.action.Action;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import qupath.ext.demo.demoExtension;
+import qupath.lib.gui.ActionTools;
+import qupath.lib.gui.QuPathApp;
+import qupath.lib.gui.QuPathGUI;
+import qupath.lib.gui.tools.GuiTools;
+
+import java.io.File;
+
+/**
+ * Demo Commands
+ */
+@SuppressWarnings("javadoc")
+public class DemoCommands {
+    Logger logger = LoggerFactory.getLogger(DemoCommands.class);
+    @ActionTools.ActionMenu("Demo>DemoCommand-1>")
+    @ActionTools.ActionDescription("Demo command-1 description")
+    public final Action actionDemo;
+
+    public DemoCommands(QuPathGUI qupath) {
+        actionDemo = ActionTools.actionBuilder("Open directory",
+                        e -> {
+                            // Try to reveal directory in Finder/Windows Explorer etc.
+                            File dir = new File("C:\\Apps");
+                            if (!dir.exists()) {
+                                dir.mkdir();
+                                logger.debug("test logger");//qupath.toString());
+
+                            }
+                            GuiTools.openFile(dir);
+                        }
+                )
+                .disabled(false)
+                .longText("Open the script directory outside QuPath.")
+                .build();
+    }
+}
---
+++ src/main/java/qupath/ext/demo/demoExtension.java
@@ -0,0 +1,95 @@
+/*-
+ * Copyright 2020-2021 Your Name
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package qupath.ext.demo;
+import org.controlsfx.control.action.Action;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import qupath.ext.demo.commands.DemoCommands;
+import qupath.lib.common.Version;
+import qupath.lib.gui.ActionTools;
+import qupath.lib.gui.QuPathGUI;
+import qupath.lib.gui.extensions.QuPathExtension;
+import qupath.lib.gui.tools.GuiTools;
+
+import java.io.File;
+
+import static qupath.lib.gui.ActionTools.getAnnotatedActions;
+
+/**
+ * Install demo extension.
+ * <p>
+ * This is a demo extension with menu entry and a command entry
+ * Next version may have a sample dialog.
+ * Based on Pete's StarDist extension
+ * @author Ajay Zalavadia
+ */
+public class demoExtension implements QuPathExtension {
+
+	private final static Logger logger = LoggerFactory.getLogger( demoExtension.class);
+
+	/**
+	 * Demo Commands
+	 */
+	@SuppressWarnings("javadoc")
+	public static class DemoCommand {
+		@ActionTools.ActionMenu("Demo>DemoCommand>")
+		@ActionTools.ActionDescription("Demo command description")
+		public final Action actionDemo;
+
+		public DemoCommand(QuPathGUI qupath) {
+			actionDemo = ActionTools.actionBuilder("Open directory",
+							e -> {
+								// Try to reveal directory in Finder/Windows Explorer etc.
+								File dir = new File("C:\\Apps");
+								if (!dir.exists()) {
+									dir.mkdir();
+									logger.debug(qupath.toString());
+
+								}
+								GuiTools.openFile(dir);
+							}
+					)
+					.disabled(false)
+					.longText("Open the script directory outside QuPath.")
+					.build();
+		}
+	}
+
+	@Override
+	public void installExtension(QuPathGUI qupath) {
+		// Does nothing
+		logger.debug("Installing extension");
+		//qupath.installActions(getAnnotatedActions(new DemoCommand(qupath)));
+		qupath.installActions(getAnnotatedActions(new DemoCommands(qupath)));
+	}
+
+	@Override
+	public String getName() {
+		return "Demo extension";
+	}
+
+	@Override
+	public String getDescription() {
+		return "Sample description of the demo extension";
+	}
+	
+	@Override
+	public Version getQuPathVersion() {
+		return Version.parse("0.3.0");
+	}
+
+}
